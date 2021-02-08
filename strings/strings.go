415a2d0974a5123938dbc0021230a757678d0cb9
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2021-02-08 22:41:22 +0900

    Add Capitalize close #4

--- strings/strings.go
+++ strings/strings.go
@@ -83,6 +83,18 @@ func Length(s string) int {
 	return utf8.RuneCountInString(s)
 }
 
+// Capitalize Return a copy of the string with its first character capitalized and the rest lowercased.
+func Capitalize(s string) string {
+	length := Length(s)
+	if length == 0 {
+		return s
+	} else if length == 1 {
+		return strings.ToUpper(s)
+	}
+	r, size := utf8.DecodeRuneInString(s)
+	return string(unicode.ToUpper(r)) + strings.ToLower(s[size:])
+}
+
 // Count Return the number of non-overlapping occurrences of substring sub in the range [start, end]. Optional arguments start and end are interpreted as in slice notation.
 func Count(s, sub string, options ...Option) int {
 	arg := Arg{
